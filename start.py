52de54b4a3f60a389f00b3b0de06149a8e265466
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2022-04-23 17:00:25 +0200

    Konwersja typow

---
+++ start.py
@@ -0,0 +1,30 @@
+print('Bartosz Rzemek')
+
+x = 3 #integer
+f = 3.24 #float
+name = "Python" #string
+arr = ["apple", "banana", "cherry"] #list
+
+dict = {"name" : "John", "age" : 36, "maria" : 33} #dict
+
+logic = True #boolean
+
+name = name[1:4]
+
+x = 1/3
+sum =  x + f
+
+age = str(dict["age"])
+
+johnAge = age + " John"
+
+age = int(age) + 1
+
+maria = list(dict)[2]
+
+
+print(age)
+
+# sumInteger = (int(x) + int(f))
+# print(shortName)
+# print(sumInteger)
