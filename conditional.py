519b6967fe63fbc2326cb11530fad73feb3661d1
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2022-04-24 10:28:00 +0200

    If else

---
+++ conditional.py
@@ -0,0 +1,28 @@
+# s = "What is your age?:"
+# index = s.find("is")
+# print(index)
+
+# if "is" in s:
+#     print("Is in s string")
+
+# if True:
+#     print("Prawda!")
+
+# a = True
+# b = True
+# c = False
+
+# if a and b and c:
+#     print("Operator and : Prawda!")
+
+a = 50
+b = 5
+
+if a < b:
+    print(str(a) + "<" + str(b))
+elif a > b:
+    print("%s > %s" % (a,b))
+elif a == b:
+    print("a == b")
+else:
+    print("False!")
--- start.py
+++ start.py
@@ -1,30 +1,25 @@
 print('Bartosz Rzemek')
-
 x = 3 #integer
 f = 3.24 #float
 name = "Python" #string
 arr = ["apple", "banana", "cherry"] #list
-
 dict = {"name" : "John", "age" : 36, "maria" : 33} #dict
-
 logic = True #boolean
-
 name = name[1:4]
-
 x = 1/3
 sum =  x + f
-
 age = str(dict["age"])
-
 johnAge = age + " John"
+johnAge2 = "John age is: %s, x : %s" % (age, x)
 
+print('Age: %s, X: %s' % (age, x))
 age = int(age) + 1
-
 maria = list(dict)[2]
+ageFromUser  = input("What is your age?:")
+ageFromUserInt = int(ageFromUser)
+print("Your age is: %s" % ageFromUser)
 
 
-print(age)
+sumInteger = (int(x) + int(f))
 
-# sumInteger = (int(x) + int(f))
-# print(shortName)
-# print(sumInteger)
+print(sumInteger)
