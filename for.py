cf71e4ad2ea9f7a09521c4497d35e7f9a6234149
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2022-04-24 11:04:09 +0200

    for

---
+++ for.py
@@ -0,0 +1,25 @@
+# fruits = ["apple", "banana", "cherry", "apricote", "banana"]
+
+# for fruit in fruits:
+#     if fruit == "cherry":
+#         break
+#     print(fruit)
+
+# for fruit in fruits:
+#     print(fruit)
+#     if fruit == "banana":
+#         break
+
+# for x in "cokolwiek":
+#     print(x)
+
+# for x in range(2, 10):
+#     print(x)
+
+for x in range(5):
+    for y in range(5):
+        for z in range(5):  
+            print("%s,%s,%s" % (x,y,z))
+
+
+
